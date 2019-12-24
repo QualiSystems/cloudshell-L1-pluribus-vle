cef222568962ef8367d8036d0554ce56409b64c1
Yaroslav Nikonorov <yaroslav.n@quali.com>
2019-12-24 17:33:27 +0200

    Fixed tests

--- tests/pluribus_vle/test_driver_commands.py
+++ tests/pluribus_vle/test_driver_commands.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from mock import Mock
 
 from cloudshell.layer_one.core.driver_commands_interface import DriverCommandsInterface
-from pluribus_netvisor_vle.driver_commands import DriverCommands
+from pluribus_vle.driver_commands import DriverCommands
 
 
 
